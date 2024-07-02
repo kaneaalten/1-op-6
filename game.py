b52304d4213d0633c7c7e9c6e46eeb13bcd6c8b8
kaneaalten <143386669+kaneaalten@users.noreply.github.com>
2024-07-02 14:57:03 +0200

    feat: add init tkinter

---
+++ game.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+class MainApp:
+    def __init__(self, master):
+        self.master = master
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = MainApp(root)
+    root.mainloop()
